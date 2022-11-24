8c3100b9d9b049a60b57f46b3c7799d3e5447a17
BlagoKolev <blagovest.kolev@gmail.com>
2022-11-24 10:29:33 +0200

    Customize table and button.

--- inensia frontend/script.js
+++ inensia frontend/script.js
@@ -1,67 +1,71 @@
-async function CreateTable(){
+//  Usefull links :
+//  http://xpather.com/
+//  https://developer.mozilla.org/en-US/docs/Web/API/XPathResult
+
+async function CreateTable() {
 
     let xml = await fetch("https://test.ce2s.net/Study.xml")
-    .then(response => response.text())
-    // .then(str => str.parseFromString(str, "text/xml"))
-    .then(xml => {
-        //console.log(xml)
-        let parser = new DOMParser();
-        let xmlDom = parser.parseFromString(xml, 'application/xml')
-        let a = xmlDom.evaluate('/Result/Item/name', xmlDom, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-       // console.log(a.textContent)
-        return xmlDom;
-    })
-    .catch(ex => ex.message);
+        .then(response => response.text())
+        // .then(str => str.parseFromString(str, "text/xml"))
+        .then(xml => {
+            //console.log(xml)
+            let parser = new DOMParser();
+            let xmlDom = parser.parseFromString(xml, 'application/xml')
+            let a = xmlDom.evaluate('/Result/Item/name', xmlDom, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            // console.log(a.textContent)
+            return xmlDom;
+        })
+        .catch(ex => ex.message);
 
-//get values from XML using xPath
-let sysElement = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item[2]/Relationships/Item/keyed_name', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
- 
-let sysElementItem ='Mirror1';
-let edcUsed = 'Surface Offset 1';
-let elemRequirement = '1CB7975079AA472A8E1B380FA808AE8F';
-let conditionExpression = 'val > 0.25';
+    //get values from XML using xPath
+    let sysElement = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item[2]/Relationships/Item/keyed_name', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
 
-let reqTitle = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/req_title', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
+    let sysElementItem = 'Mirror1';
+    let edcUsed = 'Surface Offset 1';
+    let elemRequirement = '1CB7975079AA472A8E1B380FA808AE8F';
+    let conditionExpression = 'val > 0.25';
 
-let systemUsed = xml.evaluate('/Result/Item/name', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
+    let reqTitle = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/req_title', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
 
-let studyName = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/name', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
+    let systemUsed = xml.evaluate('/Result/Item/name', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
+
+    let studyName = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/name', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
 
 
-let studyState = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/state', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
+    let studyState = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/state', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
 
-let task = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/keyed_name', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
+    let task = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/keyed_name', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
 
-let resultName = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships//Item/Relationships/Item/ar_resultname', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
+    let resultName = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships//Item/Relationships/Item/ar_resultname', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
 
-let resultValue = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships//Item/Relationships/Item/ar_resultvalue', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
+    let resultValue = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships//Item/Relationships/Item/ar_resultvalue', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
 
-let resultUnit = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/ar_resultunit', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
+    let resultUnit = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/ar_resultunit', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
 
-let targetMet = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/ar_target_met', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
+    let targetMet = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/ar_target_met', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
 
-let resultConditionExpression = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/ar_conditionexp', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
+    let resultConditionExpression = xml.evaluate('/Result/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/Relationships/Item/ar_conditionexp', xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
 
-//create an array with fetched data
-const fetchedDataArray = [
-    sysElement,
-    sysElementItem,
-    edcUsed,
-    elemRequirement,
-    conditionExpression,
-    reqTitle,
-    systemUsed,
-    studyName,
-    studyState,
-    task,
-    resultName,
-    resultValue,
-    resultUnit,
-    targetMet,
-    resultConditionExpression
-];
+    //create an array with fetched data
+    const fetchedDataArray = [
+        sysElement,
+        sysElementItem,
+        edcUsed,
+        elemRequirement,
+        conditionExpression,
+        reqTitle,
+        systemUsed,
+        studyName,
+        studyState,
+        task,
+        resultName,
+        resultValue,
+        resultUnit,
+        targetMet,
+        resultConditionExpression
+    ];
 
-//create an array with table columns 
+    //create an array with table columns 
     let columnsArray = [
         'SysElement',
         'Sys Element Item',
@@ -84,7 +88,7 @@ const fetchedDataArray = [
     let container = document.getElementById('container');
     let table = document.createElement('table');
     table.setAttribute('id', 'table');
-    table.className = 'table';
+    table.className = 'table table-hover shadow p-3 mb-5 bg-white rounded';
     let thead = document.createElement('thead');
     let tbody = document.createElement('tbody');
 
@@ -93,7 +97,7 @@ const fetchedDataArray = [
     table.appendChild(tbody);
 
     let tableHeadRow = document.createElement('tr');
-    tableHeadRow.setAttribute('id','head-row');
+    tableHeadRow.setAttribute('id', 'head-row');
     thead.appendChild(tableHeadRow);
 
     //create table body
@@ -101,21 +105,20 @@ const fetchedDataArray = [
     tableBodyRow.setAttribute("id", "body-row");
     tbody.appendChild(tableBodyRow);
 
-  //add columns to table head and cells to table body
-  for (let index = 0; index < columnsArray.length; index++) {
-    let thElement = document.createElement('th');
-    thElement.className = 'cell';
-    thElement.textContent = columnsArray[index];
-    tableHeadRow.appendChild(thElement);
+    //add columns to table head and cells to table body
+    let color = 'table-success';
+    for (let index = 0; index < columnsArray.length; index++) {
+        index%2==0 ? color = 'table-danger' : color='table-info'; 
+        let thElement = document.createElement('th');
+        thElement.className = `cell ${color}`;
+        thElement.textContent = columnsArray[index];
+        tableHeadRow.appendChild(thElement);
 
-    let tdElement = document.createElement('td');
-    tdElement.className = 'cell';
-    tdElement.textContent = fetchedDataArray[index];
-    tableBodyRow.appendChild(tdElement);
-}
-
-
-
+        let tdElement = document.createElement('td');
+        tdElement.className = `cell ${color}`;
+        tdElement.textContent = fetchedDataArray[index];
+        tableBodyRow.appendChild(tdElement);
+    }
 }
 
 CreateTable();
